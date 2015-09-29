62b0c9c6bdd7b12a3244e7b6329871a3df6d9629
kevindy <kevinmarlson@gmail.com>
2015-09-29 14:18:54 +0800

    Hello Submission -Kevin

---
+++ app/src/main/java/com/example/kevinmarlsondy/hellokevin/Two.java
@@ -0,0 +1,18 @@
+package com.example.kevinmarlsondy.hellokevin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Kevin Marlson Dy on 9/22/2015.
+ */
+public class Two extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Two);
+    }
+
+}
